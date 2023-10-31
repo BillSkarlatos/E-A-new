93c9d58b5b62c979942888aecd527bad41038e1e
Bill_Sk <skarlatosbill@gmail.com>
2023-10-31 13:32:53 +0200

    Setup/ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Entimoi Apateones. Xanthi,Greece
+
+Subsites:
+-Landing Site
+-Apply Site
+-Who we are
+-Upcoming Show
+-Past shows
+-Contact Us
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 
+// navbar
 document.addEventListener('DOMContentLoaded', function() {
     const navbarPlaceholder = document.getElementById('navbar-placeholder');
     const navbarRequest = new XMLHttpRequest();
@@ -10,14 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     navbarRequest.send();
   });
-
+// Hero
 document.addEventListener('DOMContentLoaded', function() {
-    const navbarPlaceholder = document.getElementById('navbar-placeholder');
-    const navbarRequest = new XMLHttpRequest();
-    navbarRequest.open('GET', 'navbar.html', true);
-    navbarRequest.onreadystatechange = function() {
-      if (navbarRequest.readyState === 4 && navbarRequest.status === 200) {
-        navbarPlaceholder.outerHTML = navbarRequest.responseText;
+    const heroPlaceholder = document.getElementById('hero-placeholder');
+    const heroRequest = new XMLHttpRequest();
+    heroRequest.open('GET', 'hero.html', true);
+    heroRequest.onreadystatechange = function() {
+      if (heroRequest.readyState === 4 && heroRequest.status === 200) {
+        heroPlaceholder.outerHTML = heroRequest.responseText;
       }
     };
     navbarRequest.send();
